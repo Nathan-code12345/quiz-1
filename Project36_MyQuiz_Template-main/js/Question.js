0018587aad570d2b647fd4f3500dcf3f0c616500
Nathan-code12345 <83987026+Nathan-code12345@users.noreply.github.com>
2021-12-12 18:03:46 -0800

    Add files via upload

---
+++ Project36_MyQuiz_Template-main/js/Question.js
@@ -0,0 +1,74 @@
+class Question {
+
+  constructor() {
+    this.title = createElement('h1')
+    this.input1 = createInput("Enter Your Name Here....");
+   //Create a input box to enter the number
+   this.input2 = createInput("type");
+
+
+    this.button = createButton('Submit');
+    //Create elements for One question (h3) and 4 options (h4)
+
+    this.question = createElement('h3');
+    this.option1 = createElement('h4');
+    this.option2 = createElement('h4');
+    this.option3 = createElement('h4');
+    this.option4 = createElement('h4');
+   
+    this.message = createElement("h2")
+  }
+
+  hide(){
+    this.title.hide();
+    this.input1.hide();
+    this.button.hide();
+   
+    this.message.hide();
+    //Add hide() for questions, options & input box
+    this.input2.hide();
+    
+
+
+
+
+  }
+
+  display(){
+    this.title.html("Vocabulary Quiz");
+    this.title.position(350, 0);
+
+
+    //Create html() and position() for each question, input and answers.
+    this.question.html("Define and use each word in a sentence. " );
+    this.question.position(150, 80);
+    this.option1.html("1: Blatherskite " );
+    this.option1.position(150, 100);
+    this.option2.html("2:Prevarication" );
+    this.option2.position(150, 120);
+    this.option3.html("3: Interrogatory " );
+    this.option3.position(150, 140);
+    this.option4.html("4: Antediluvian" );
+    this.option4.position(150, 160);
+
+    this.input2.position(350, 230);
+    this.input1.position(150, 230);
+    this.button.position(290, 300);
+
+    // Add a mousepressed action when the button is clicked Display the this.message
+    this.button.mousePressed(()=>{
+      this.title.hide();
+      this.input1.hide();
+      this.input2.hide();
+      this.button.hide();
+      this.option1.hide();
+      this.option2.hide();
+      this.option3.hide();
+      this.option4.hide();
+      this.question.hide();
+    })
+
+
+
+  }
+}
